ea8a8b2deb377dd6f22d8c0a722564d7b5e2f6c4
vishusahab <rockyvarun42@gmail.com>
2021-11-16 14:29:26 +0530

    Update data.js

--- src/Homepage/data.js
+++ src/Homepage/data.js
@@ -1,8 +1,11 @@
  const data = [
-    {
+   [ {
         "id" : 1,
-        "name" : "Fund 1",
+        "name" : "Schroder Uk",
+        'latest':33,
+        "gl":809,
         "value" : "100",
+        "quantity":1333,
         "currency" : "USD",
         "date" : "01/01/2019",
         "description" : "This is a description for fund 1"
@@ -10,32 +13,39 @@
     },
    {
     "id" : 2,
-        "name" : "Fund 2",
+        "name" : "TM Stonehage",
+        'latest':1371,
+        "gl":809,
+        "quantity":1333,
         "value" : "200",
         "currency" : "USD",
         "date" : "01/01/2019",
         "description" : "This is a description for fund 2"
     
-    },
-  {
-    "id" : 3,
-        "name" : "Fund 3",
-        "value" : "300",
+    }],
+    [{
+        "id" : 3,
+        "name" : "Schroder US",
+        'latest':33,
+        "gl":809,
+        "value" : "100",
+        "quantity":1333,
         "currency" : "USD",
         "date" : "01/01/2019",
         "description" : "This is a description for fund 3"
     
-    
     },
-{
-    "id" : 4,    
-    "name" : "Fund 4",
-        "value" : "400",
+   {
+    "id" : 4,
+        "name" : "BM Stonehage",
+        'latest':1371,
+        "gl":809,
+        "quantity":1333,
+        "value" : "200",
         "currency" : "USD",
         "date" : "01/01/2019",
         "description" : "This is a description for fund 4"
     
-    
-    },
+    }],
 ]
 export default data;
