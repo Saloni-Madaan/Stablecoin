73bc7a3fba5c15b3f263f1dbff2cde8f69c6f35e
vishusahab <rockyvarun42@gmail.com>
2021-11-17 13:16:35 +0530

    Update data.js

--- src/Homepage/data.js
+++ src/Homepage/data.js
@@ -5,7 +5,22 @@ const Data = [
         "value" : "231.87 USD",
         "currency" : "21.87 USD",
         "date" : "01/01/2019",
-        "description" : "This is a description for fund 1"
+        "description" : "This is a description for fund 1",
+        "stocks":[{
+            name: 'Schroder Uk',
+            label: 35,
+            price: 1330,
+            quantity: '439/400',
+            gl: 809.9
+          },
+          {
+            name: 'TM Stockage',
+            label: 1371,
+            price: 72.39,
+            quantity: '439/400',
+            gl: 809.9
+          }
+        ]
     
     },
    {
@@ -14,7 +29,22 @@ const Data = [
         "value" : "11,593.61 USD",
         "currency" : "1,093 USD",
         "date" : "01/01/2019",
-        "description" : "This is a description for fund 2"
+        "description" : "This is a description for fund 2",
+        "stocks":[{
+            name: 'Schroder US',
+            label: 35,
+            price: 1330,
+            quantity: '439/400',
+            gl: 809.9
+          },
+          {
+            name: 'PM Stockage',
+            label: 1371,
+            price: 72.39,
+            quantity: '439/400',
+            gl: 809.9
+          }
+        ]
     
     },
   {
@@ -23,7 +53,22 @@ const Data = [
         "value" : "104,342.45 USD",
         "currency" : "9,842.45 USD",
         "date" : "01/01/2019",
-        "description" : "This is a description for fund 3"
+        "description" : "This is a description for fund 3",
+        "stocks":[{
+            name: 'Schroder Uk',
+            label: 35,
+            price: 1330,
+            quantity: '439/400',
+            gl: 809.9
+          },
+          {
+            name: 'TM Stockage',
+            label: 1371,
+            price: 72.39,
+            quantity: '439/400',
+            gl: 809.9
+          }
+        ]
     
     
     },
@@ -33,7 +78,22 @@ const Data = [
         "value" : "3,478.00 USD",
         "currency" : "328.08 USD",
         "date" : "01/01/2019",
-        "description" : "This is a description for fund 4"
+        "description" : "This is a description for fund 4",
+        "stocks":[{
+            name: 'Schroder Uk',
+            label: 35,
+            price: 1330,
+            quantity: '439/400',
+            gl: 809.9
+          },
+          {
+            name: 'TM Stockage',
+            label: 1371,
+            price: 72.39,
+            quantity: '439/400',
+            gl: 809.9
+          }
+        ]
     
     
     },
