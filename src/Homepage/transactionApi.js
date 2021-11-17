e79f1578bc3b3dd257ad98e8b01f051122d5df70
vishusahab <rockyvarun42@gmail.com>
2021-11-17 12:52:59 +0530

    Update transactionApi.js

--- src/Homepage/transactionApi.js
+++ src/Homepage/transactionApi.js
@@ -16,7 +16,14 @@ const Transaction = [
         paidAmount: 50,
         state: "success",
         date: "11-09-2021",
-        address : "0x63f32ab904f3DA35d9922Bf3763318bfC13EcA46"
+        address : "0x63f32ab904f3DA35d9922Bf3763318bfC13EcA46",
+        userWalletAddress : "f376331",
+        wallet:{
+          _id: "06f153af-2367-4dd6-a6bf-ebe199616041",
+          address: "0x47B39d95105BC7fe6d7A0172FF83c0Db4f792e4F",
+          
+        },
+        // _id : ""
 
     },
     {
@@ -36,7 +43,13 @@ const Transaction = [
         paidAmount:150,
         state: "pending",
         date: "11-09-2021",
-        address : "0x63f32ab904f3DA35d7652Bf3763318bfC13EcA46"
+        address : "0x63f32ab904f3DA35d7652Bf3763318bfC13EcA46",
+        userWalletAddress : "f376331",
+        wallet:{
+          _id: "06f153af-2367-4dd6-a6bf-ebe199616041",
+          address: "0x47B39d95105BC7fe6d7A0172FF83c0Db4f792e4F",
+          
+        },
     },
 
 ]
