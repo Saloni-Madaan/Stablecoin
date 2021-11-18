c710c4d41a4ab1a383b23f524b16f7e7d38c8293
vishusahab <rockyvarun42@gmail.com>
2021-11-18 12:40:25 +0530

    Create checkout.js

---
+++ src/Homepage/checkout.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Checkout() {
+  return (
+    <Box >
+      <Typography variant="h3" textAlign="left" >
+        Buy, Sell, Switch
+        <Typography variant="subtitle2"  textAlign="right">
+        Prices and valuations updated at 14:08 GMT on 17/11/2021
+      </Typography>
+      </Typography>
+    </Box>
+  );
+}
