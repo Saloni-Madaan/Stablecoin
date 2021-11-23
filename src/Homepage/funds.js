9b13e0cd75684abf58daab63b9c919041186cf09
vishusahab <rockyvarun42@gmail.com>
2021-11-23 20:51:20 +0530

    Update funds.js

--- src/Homepage/funds.js
+++ src/Homepage/funds.js
@@ -33,6 +33,13 @@ const useStyles = makeStyles((theme) => ({
     // border: "1px solid black",
     padding: "3px",
   },
+  td1 : {
+    height: "30px",
+    width: "10px",
+    textAlign: "center",
+    // border: "1px solid black",
+    padding: "0px",
+  },
 }));
 
 const Funds = () => {
@@ -42,30 +49,31 @@ const Funds = () => {
     <>
       <Container className={classes.root}>
         <TableContainer component={Paper}>
-          <Table sx={{borderTop: '1px solid red'}}>
-            {/* <TableHead>
-              <TableRow>
-                <TableCell>
-                  <b>Name</b>
-                </TableCell>
-                <TableCell>
-                  <b>Description</b>
-                </TableCell>
-                <TableCell>
-                  <b>Amount</b>
-                </TableCell>
-                <TableCell>
-                  <b>Currency</b>
-                </TableCell>
-                <TableCell>
-                  <b>Date</b>
-                </TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead> */}
+          <Table>
+            <TableHead justify="center" className={classes.td1}>
+            <TableRow  sx={{'borderTop': 0 , 'borderRight': 0, 'borderBottom': 0, 'borderColor': '#232328','borderLeftWidth': "12px",'borderBottomWidth':"20px",'borderBottomColor':"#F2F0F4",borderStyle:"solid"}}>
+            <TableCell style={{ marginLeft: 1000 }}>
+                    <h2 style={{ font: "10" }, { color: "#146BAB" }}>View all investments  </h2>
+                   
+                  </TableCell>
+                  <TableCell ><b style={{fontSize:20,color:"white"}}>Investments + total cash & coins</b>
+                    </TableCell>
+
+                  <TableCell><b style={{fontSize:20,color:"white"}}>Investments + total cash & coins</b>
+                    </TableCell>
+                  <TableCell>
+                    <h3  style={{
+                      flexDirection: "row",
+                      justifyContent: "flex-end"
+                    }}><Link  style={{ textDecoration: 'none' }} to={{pathname:"dashboard/transactions"}}><ArrowForwardIosIcon fontSize="large" color="primary"/></Link>  </h3>
+                  </TableCell>
+         </TableRow>
+            </TableHead>
             <TableBody justify="center" className={classes.td}>
               {data.map((curElem, index) => (
-                <TableRow key={index} sx={{borderTop: 0 , borderRight: 0, borderBottom: 0, borderColor: '#000000',borderLeftWidth: "10px"}}>
+                
+                <TableRow key={index}   sx={{'borderTop': 0 , 'borderRight': 0, 'borderBottom': 0, 'borderColor': '#40BEEA','borderLeftWidth': "12px",'borderBottomWidth':"30px",'borderBottomColor':"#F2F0F4",borderStyle:"solid"}}>
+                  
                   {/* <TableCell>
                     <b>{curElem.name}</b>
                   </TableCell>
@@ -75,7 +83,7 @@ const Funds = () => {
                   <TableCell>{curElem.date}</TableCell> */}
                   <TableCell style={{ marginLeft: 1000 }}>
                     <b>Fidelity Personal Investing accounts</b> <br></br>
-                    <h2 style={{ font: "10" }, { color: "#006193" }}>  {curElem.name} </h2>
+                    <h2 style={{ font: "10" }, { color: "#146BAB" }}>  {curElem.name} </h2>
                     AN123456789
                   </TableCell>
 
